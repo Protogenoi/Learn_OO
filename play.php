ebaca7e0b447ba14f4f4ebefdc653df1a68c6adf
Michael <michael@adl-crm.uk>
2018-10-04 14:10:17 +0100

    Completed Course 1: Chapter 3: Class Methods

--- play.php
+++ play.php
@@ -5,9 +5,34 @@ class Ship
 
     public $name;
 
+    public $weaponPower = 0;
+
+    public $jediPower = 0;
+
+    public $strength = 0;
+
+    public function sayHello()
+    {
+        echo "hello";
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
 }
 
 $myShip = new Ship();
 $myShip->name = 'Jedi Starship';
+$myShip->weaponPower = 10;
 
 echo 'Ship name: ' . $myShip->name;
+
+
+echo '<hr/>';
+$myShip->sayHello();
+echo '<hr/>';
+echo $myShip->getName();
+echo '<hr/>';
+var_dump($myShip->weaponPower);
