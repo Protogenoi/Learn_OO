5909a789733e3835e9ed0df68d18feda8ed73d6a
Michael <michael@adl-crm.uk>
2018-10-08 14:59:15 +0100

    Completed Course 2: Chapter 11: Service Container

---
+++ lib/Container.php
@@ -0,0 +1,30 @@
+<?php
+
+class Container
+{
+
+    public function __construct(array $configuration)
+    {
+
+        $this->configuration = $configuration;
+
+    }
+
+    /**
+     * @return PDO
+     */
+
+    public function getPDO()
+    {
+
+        $pdo = new PDO(
+            $this->configuration['db_dsn'],
+            $this->configuration['db_user'],
+            $this->configuration['db_pass']
+        );
+
+        return $pdo;
+
+    }
+
+}
